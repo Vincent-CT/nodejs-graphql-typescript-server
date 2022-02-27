8001c5bc2929b923d291998e43063acea93ca1ce
Vincent Tran <vince1@live.dk>
2022-02-27 16:45:25 +0100

    initial-setup add jest, eslint, prettier and nodemon

---
+++ src/__tests__/math_test/add.test.ts
@@ -0,0 +1,7 @@
+import add from '../../math/add';
+
+describe('this is a test', () => {
+    it('Should pass', () => {
+        expect(add(1, 2)).toBe(3);
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import add from './math/add';
+import 'dotenv/config';
+
+console.log(add(2, 4));
+console.log(process.env.PORT);
